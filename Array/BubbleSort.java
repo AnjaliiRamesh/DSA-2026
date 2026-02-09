cbd86f1c47fe408a597c250ea878cdec2d7fed66
Anjali <anjaliramesh14012005@gmail.com>
2026-02-09 19:09:38 +0530

    BubbleSorting algorithm

---
+++ Array/BubbleSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int arr[] = {3,2,4};
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++){
+            for (int j = 0; j < n - i - 1; j++){
+                if (arr[j] > arr[j + 1]) {
+                  
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        
+        System.out.println(Arrays.toString(arr));
+
+        
+    }
+}
+    
+
