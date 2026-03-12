a49ab142205641d9ffa89a15a549713c6597a91b
Anjali <anjaliramesh14012005@gmail.com>
2026-03-12 09:42:27 +0530

    math

--- Array/TwoSum.java
+++ Array/TwoSum.java
@@ -1,15 +1,18 @@
 public class TwoSum{
 public static void main(String[] args) {
-    int arr[] = {3,3};
-    int tar = 6;
-
+    int arr[] = {2,7,11,15};
+    int tar = 9;
+    int n = arr.length;
+    
     for(int i=0; i<arr.length; i++){
         for(int j=i+1; j<arr.length; j++){
             if(arr[i]+arr[j] == tar){
                 System.out.println(i + " "+j);
             }
         }
+        //add sum
         
+
 }
 }
 }
