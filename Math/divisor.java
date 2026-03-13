0ec523c85918a783b50bd8022d344d0d4e519007
Anjali <anjaliramesh14012005@gmail.com>
2026-03-13 08:26:57 +0530

    optimised divisores of a number

--- Math/divisor.java
+++ Math/divisor.java
@@ -1,10 +1,24 @@
 class divisor{
     public static void main(String[] args) {
         int n = 20;
-        for(int i = 1; i<=n;i++){
+        if(n==1){
+            System.out.print(n);
+        }
+         if(n > 1){
+            if(n%2!=0){
+            System.out.print( 1 + " " + n);
+        }
+        else{
+            for(int i = 1; i<=n/2;i++){
             if(n%i==0){
                 System.out.print(i + " ");
             }
         }
+        System.out.print(n);
+        }
+        }
+        
+        
+        
     }
 }
