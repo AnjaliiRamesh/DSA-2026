5cb4db3bfe6861902704a1668399ff4cfee7aea4
Anjali <anjaliramesh14012005@gmail.com>
2026-04-12 14:45:25 +0530

    java basics

---
+++ javaBasics/Swap.java
@@ -0,0 +1,12 @@
+public class Swap{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("Before swap: a = " + a + ", b = " + b);
+        //swap without using third variable
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("After swap: a = " + a + ", b = " + b);
+    }
+}
