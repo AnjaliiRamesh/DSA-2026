7a56226cb2202ca27a2926edb6cf70f44daf4840
Anjali <anjaliramesh14012005@gmail.com>
2026-04-14 15:10:54 +0530

    prime numbers

---
+++ javaBasics/PrimeOrNot.java
@@ -0,0 +1,23 @@
+public class PrimeOrNot {
+    public static void main(String[] args) {
+        int n = 29;
+        if(n<=1){
+            System.out.println(n + " is not a prime number");
+        }
+        for(int i=2; i<n; i++){
+             if(n%i==0){
+                System.out.println(n + " is not a prime number");
+                break;
+            }else{
+                System.out.println(n + " is a prime number");
+                break;
+            }
+        }
+        
+        };
+        }
+        
+       
+    
+    
+
--- readme.md
+++ readme.md
@@ -15,3 +15,14 @@ int age = sc.nextInt();
 Reads the entire line (including spaces)
 Stops only when it finds a newline (Enter)
 String fullName = sc.nextLine();
+
+# Prime Number
+
+What is a Prime Number 
+
+A number is prime if:
+It is greater than 1
+It has only 2 factors → 1 and itself
+
+👉 So you must check:
+Does any number between 2 and n-1 divide n?
